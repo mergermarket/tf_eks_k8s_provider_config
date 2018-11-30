fe7ba3aee79538f3550c56e081a4db9291227e89
Tom Yandell <tom@tomyandell.co>
2018-11-30 12:11:37 +0000

    Add README and minor interface modification
    Make it so the outputs match up with the provider inputs to make it
    easier to use.

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-output "endpoint" {
+output "host" {
   value = "${data.aws_eks_cluster.eks_cluster.endpoint}"
 }
 
-output "ca_certificate" {
+output "cluster_ca_certificate" {
   value = "${base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)}"
 }
 
